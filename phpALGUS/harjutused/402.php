9ad9df74a3b5e8d40fe4b4ab4461f473abc36447
DariaEvtina <77333298+DariaEvtina@users.noreply.github.com>
2021-11-09 10:55:22 +0200

    Ülesanne 402 on tehtud

--- phpALGUS/harjutused/402.php
+++ phpALGUS/harjutused/402.php
@@ -17,23 +17,19 @@
 ?>
 <h2>2. 20 Tekstkasti sükliga</h2>
 <?php
-$a=1;
-$i1=21;
-while($i1<=40){
-    echo "<input type='text' id='$i1' name='text[]' >";
-    echo "<label for='$i1'>tekstkast ".$a."</label>"."<br>";
-    $a++;
+$i1=1;
+while($i1<=20){
+    echo "<input type='text' id=t'$i1' name='text[]' >";
+    echo "<label for='$i1'>cell ".$i1."</label>"."<br>";
     $i1++;
 }
 ?>
 <h2>2. 20 Radionupud sükliga</h2>
 <?php
-$b=1;
-$i2=41;
-while($i2<=60){
-    echo "<input type='radio' id='$i2' name='radio[]' value='$b'>";
-    echo "<label for='$i2'>radionup ".$b."</label>"."<br>";
-    $b++;
+$i2=1;
+while($i2<=20){
+    echo "<input type='radio' id=r'$i2' name='radio[]' value='$i2'>";
+    echo "<label for='$i2'>radio ".$i2."</label>"."<br>";
     $i2++;
 }
 ?>
