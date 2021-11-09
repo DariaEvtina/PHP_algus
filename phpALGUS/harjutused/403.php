127806265080e907fcc747fcf02ee8cf75cd0cc2
DariaEvtina <77333298+DariaEvtina@users.noreply.github.com>
2021-11-09 10:20:48 +0200

    Ülesanne 403 on tehtud

---
+++ phpALGUS/harjutused/403.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ülesanne 403</title>
+</head>
+<body>
+<h1>Ülesanne 403</h1>
+<?php
+$varved=array('DarkGoldenRod','DarkSlateGray','GoldenRod','GreenYellow','Ivory','Indigo','LightGreen','Maroon','Olive','Navy','OrangeRed','PaleVioletRed','RoyalBlue','RosyBrown');
+for($i =0;$i<15;$i++){
+    echo "<span style='color: $varved[$i]'> $varved[$i] </span>";
+}
+?>
+<a href="../TEST.php">Tagasi.. Ülesanne</a>
+
+</body>
+</html>
