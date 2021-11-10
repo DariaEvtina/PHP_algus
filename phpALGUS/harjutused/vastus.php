7dac136bbf9822de92233f68972275eae99ed242
DariaEvtina <77333298+DariaEvtina@users.noreply.github.com>
2021-11-10 09:55:29 +0200

    Mõistlatus ülesanne

---
+++ phpALGUS/harjutused/vastus.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Maatimatika vastused</title>
+</head>
+<body>
+<?php
+//lisada info teisest failist
+require_once ('moistatus.php');
+echo "<br> Õiged vastused olid: ";
+echo"<br>";
+echo "arv 1: ".$arv1;
+echo"<br>";
+echo"arv 2: ".$arv2;
+echo"<br>";
+echo"Linnanimi on : ".$nimi;
+?>
+
+</body>
+</html>
